afbb2fefc7b8a3524c0262a399ff39ff012f1ae4
louislat <lmm.latournerie@gmail.com>
2025-02-28 09:33:17 +0100

    initialize model from dataframes

---
+++ src/inputs/debtrank_initialization.jl
@@ -0,0 +1,14 @@
+
+using DataFrames
+using SparseArrays
+
+include("generate_weights.jl")
+include("../types.jl")
+
+function DRLstm(investments::DataFrame, capitals::DataFrame, economic_values::DataFrame)::DRLstm
+    nodes_id, investments_, capitals_, economic_values_ = encode_input_nodes(investments, capitals, economic_values)
+    W::SparseMatrixCSC = generate_weights_matrix(investments_, capitals_)
+    M::SparseMatrixCSC = generate_connection_matrix(economic_values_)
+    sort!(economic_values, :node)
+    return DRLstm(nodes_id, economic_values.weight, W, M)
+end
